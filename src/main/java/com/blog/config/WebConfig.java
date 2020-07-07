bb3d9e0bdf9bdd6f396645117b5f2d17481719a6
1329421480moon <1329421480@qq.coom>
2020-07-07 10:34:25 +0800

    myblog

---
+++ src/main/java/com/blog/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.blog.config;
+
+import com.blog.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //配置登录拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+//                拦截
+                .addPathPatterns("/admin/**")
+//                放行
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login");
+    }
+}
---
+++ src/main/java/com/blog/pojo/BlogQuery.java
@@ -0,0 +1,26 @@
+package com.blog.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Description TODO
+ * @Date 2020/7/6 17:03
+ * @Created by xinghang
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogQuery {
+
+    private boolean recommend;
+
+    private Long typeId;
+
+    private String title;
+
+}
